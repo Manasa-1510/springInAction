3c81decdf346aacbd6ba4c04f046df33d270be67
Manasa-1510 <33416570+Manasa-1510@users.noreply.github.com>
2018-03-16 11:53:15 +0530

    Add files via upload

---
+++ booksReadingList/src/main/java/booksReadingList/BooksReadingListApplication.java
@@ -0,0 +1,12 @@
+package booksReadingList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksReadingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksReadingListApplication.class, args);
+	}
+}
